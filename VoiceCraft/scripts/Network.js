4432e5185a0695d58219a6cefaec819a94aaa109
SineVector241 <flyingbirdasuka1@gmail.com>
2023-05-23 14:18:46 +1000

    Ingame server ui update.

--- VoiceCraft/scripts/Network.js
+++ VoiceCraft/scripts/Network.js
@@ -1,5 +1,6 @@
 import { HttpRequestMethod, HttpHeader, HttpRequest, http } from '@minecraft/server-net';
 import { world, system, Player } from '@minecraft/server';
+import { ModalFormData } from "@minecraft/server-ui";
 
 class Network {
     static IsConnected = false;
@@ -18,9 +19,8 @@ class Network {
         this.Port = Port;
         this.Key = Key;
 
-        const packet = new Packet();
+        const packet = new LoginPacket();
         packet.LoginKey = Key;
-        packet.Type = 0;
 
         const request = new HttpRequest(`http://${this.IP}:${this.Port}/`);
         request.setTimeout(5);
@@ -51,9 +51,8 @@ class Network {
             return;
         }
 
-        const packet = new Packet();
+        const packet = new BindingPacket();
         packet.LoginKey = this.Key;
-        packet.Type = 1;
         packet.Gamertag = PlayerObject.name;
         packet.PlayerKey = Key;
         packet.PlayerId = PlayerObject.id;
@@ -72,16 +71,128 @@ class Network {
             }
         });
     }
+
+    /**
+     * @argument {Player} PlayerObject
+     * @argument {number} ProximityDistance
+     * @argument {boolean} ProximityToggle
+     */
+    static UpdateSettings(PlayerObject, ProximityDistance, ProximityToggle)
+    {
+        if (!Network.IsConnected) {
+            PlayerObject.sendMessage("§cCould not request settings update. Server not linked!");
+            return;
+        }
+
+        const packet = new UpdateSettingsPacket();
+        packet.LoginKey = this.Key;
+        packet.Settings.ProximityDistance = ProximityDistance;
+        packet.Settings.ProximityToggle = ProximityToggle;
+        
+        const request = new HttpRequest(`http://${this.IP}:${this.Port}/`);
+        request.setTimeout(5);
+        request.setBody(JSON.stringify(packet));
+        request.setMethod(HttpRequestMethod.POST);
+        request.setHeaders([new HttpHeader("Content-Type", "application/json")]);
+        http.request(request).then(response => {
+            if (response.status == 200) {
+                PlayerObject.sendMessage("§2Updated Settings Successfully!");
+            }
+            else {
+                PlayerObject.sendMessage("§cAn error occured! Could not update settings!");
+            }
+        });
+    }
+
+    /**
+     * @argument {Player} PlayerObject
+     */
+    static ShowSettings(PlayerObject) 
+    {
+        if (!Network.IsConnected) {
+            PlayerObject.sendMessage("§cCould not request settings. Server not linked!");
+            return;
+        }
+
+        const packet = new GetSettingsPacket();
+        packet.LoginKey = this.Key;
+
+        const request = new HttpRequest(`http://${this.IP}:${this.Port}/`);
+        request.setTimeout(5);
+        request.setBody(JSON.stringify(packet));
+        request.setMethod(HttpRequestMethod.POST);
+        request.setHeaders([new HttpHeader("Content-Type", "application/json")]);
+        http.request(request).then(response => {
+            if (response.status == 200) {
+                const json = JSON.parse(response.body);
+                const settings = new ServerSettings();
+                settings.ProximityDistance = json.Settings.ProximityDistance;
+                settings.ProximityToggle = json.Settings.ProximityToggle;
+
+                new ModalFormData()
+                    .title("VoiceCraft Server Settings")
+                    .slider("Proximity Distance", 1, 80, 1, settings.ProximityDistance)
+                    .toggle("Proximity Enabled", settings.ProximityToggle)
+                    .toggle("Voice Effects (Coming Soon!)")
+                    .show(PlayerObject)
+                    .then((response) => {
+                        if(response.canceled) return;
+                        this.UpdateSettings(PlayerObject, response.formValues[0], response.formValues[1]);
+                    });
+            }
+            else {
+                PlayerObject.sendMessage("§cAn error occured!");
+            }
+        });
+    }
 }
 
-class Packet {
-    constructor() {
+class LoginPacket {
+    constructor()
+    {
         this.Type = 0;
+        this.LoginKey = "";
+    }
+}
+
+class BindingPacket {
+    constructor()
+    {
+        this.Type = 1;
         this.LoginKey = "";
         this.PlayerId = "";
         this.PlayerKey = "";
         this.Gamertag = "";
+    }
+}
+
+class UpdatePlayersPacket {
+    constructor() {
+        this.Type = 2;
+        this.LoginKey = "";
         this.Players = [];
+    }
+}
+
+class UpdateSettingsPacket {
+    constructor() {
+        this.Type = 3;
+        this.LoginKey = "";
+        this.Settings = new ServerSettings();
+    }
+}
+
+class GetSettingsPacket {
+    constructor() {
+        this.Type = 4;
+        this.LoginKey = "";
+    }
+}
+
+class ServerSettings {
+    constructor() {
+        this.ProximityDistance = 30;
+        this.ProximityToggle = true;
     }
 }
 
@@ -89,9 +200,8 @@ class Packet {
 system.runInterval(() => {
     if (Network.IsConnected) {
         const playerList = world.getAllPlayers().map(plr => ({ PlayerId: plr.id, DimensionId: plr.dimension.id, Location: { x: plr.getHeadLocation().x, y: plr.getHeadLocation().y, z: plr.getHeadLocation().z }, Rotation: plr.getRotation().y }));
-        const packet = new Packet();
+        const packet = new UpdatePlayersPacket();
         packet.LoginKey = Network.Key;
-        packet.Type = 2;
         packet.Players = playerList;
 
         const request = new HttpRequest(`http://${Network.IP}:${Network.Port}/`);
--- VoiceCraft/scripts/main.js
+++ VoiceCraft/scripts/main.js
@@ -1,17 +1,57 @@
-import { CommandSystem } from './Commands/CommandSystem';
-import { Network } from './Network';
+import { CommandSystem } from "./Commands/CommandSystem";
+import { Network } from "./Network";
+import { EntityInventoryComponent, ItemStack, world } from "@minecraft/server";
 
-CommandSystem.RegisterCommand("connect", function(params) {
+CommandSystem.RegisterCommand(
+  "connect",
+  function (params) {
     Network.Connect(params.IP, params.PORT, params.Key, params.source);
-}, {
+  },
+  {
     IP: "string",
     PORT: "integer",
-    Key: "string"
-});
+    Key: "string",
+  }
+);
 
-CommandSystem.RegisterCommand("bind", function(params) {
+CommandSystem.RegisterCommand(
+  "settings",
+  function (params) {
+    try {
+      const component = params.source.getComponent(
+        EntityInventoryComponent.componentId
+      );
+      const customItem = new ItemStack("minecraft:enchanted_book", 1);
+      customItem.nameTag = "§bVoiceCraft Server Settings";
+      customItem.setLore(["Open VoiceCraft Settings"]);
+      component.container.addItem(customItem);
+      params.source.sendMessage(
+        "You have been given an item in your inventory. Right Click/Interact with the item to open the settings UI"
+      );
+    } catch (ex) {
+      params.source.sendMessage(ex.toString());
+    }
+  },
+  {}
+);
+
+CommandSystem.RegisterCommand(
+  "bind",
+  function (params) {
     params.source.sendMessage("§eBinding...");
     Network.RequestBinding(params.Key, params.source);
-}, {
-    Key: "string"
+  },
+  {
+    Key: "string",
+  }
+);
+
+world.events.beforeItemUse.subscribe((ev) => {
+  try {
+    if (ev.item.getLore()[0] == "Open VoiceCraft Settings") {
+      Network.ShowSettings(ev.source);
+    }
+  } catch (ex) {
+    ev.source.sendMessage(ex.toString());
+  }
 });
